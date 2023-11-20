a7dc43c63a70fa95eda00d70564270de85990761
Ombuki <125642777+ElvisOmbuki@users.noreply.github.com>
2023-11-20 12:01:59 +0300

    Add files via upload

---
+++ nairoaccdata - Copy.js
@@ -0,0 +1,11 @@
+let shopItemsData = [
+    
+      {
+        id: "ioytrhndcv46",
+        name: " big5 peace ride touradventures",
+        price: "Traveling Service",
+        desc: "Why Travel With Us? ➖We offer affordable packages. ➖We allow payment on installments. ➖We offer best customer services.➖We make follow up during customer vacations until the end.➖We offer transports for group travels.➖We offer free professional photography for group travels.➖We uphold professionalism, trustworthy, integrity and customer service in our dealings. Website: www.big5peaceridetouradventures.com",
+        img: "te2.jpeg",
+      },
+
+]
