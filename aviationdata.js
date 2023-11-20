3edd93ff6492edbf109c96a8882ed5a8bba42825
Ombuki <125642777+ElvisOmbuki@users.noreply.github.com>
2023-11-20 11:51:55 +0300

    Add files via upload

---
+++ aviationdata.js
@@ -0,0 +1,26 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs75",
+      name: "Kenya airways",
+      price: "",
+      desc: "",
+      img: "airways.jpg",
+    },
+
+    {
+        id: "jfhgbvnscs789",
+        name: "Jambo jet",
+        price: "",
+        desc: " ",
+        img: "jambo.jpg",
+      },
+
+      {
+        id: "jfhgbvnscs776",
+        name: "Safari link",
+        price: "",
+        desc: "",
+        img: "safari.jpg",
+      },
+    
+  ];
